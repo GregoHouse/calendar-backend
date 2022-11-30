fa1288a83acd65b64f1564f382405955ac3a51c6
GregoHouse <96206226+GregoHouse@users.noreply.github.com>
2022-11-30 15:28:03 -0300

    index route updated

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ app.use( express.json() );
 app.use( '/api/auth', require('./routes/auth') );
 app.use( '/api/events', require('./routes/events') );
 
+// Ruta generica comodin 
+app.use('*', (req, res) => {
+    res.sendFile( __dirname + '/public/index.html' );
+});
+
 // Levantar servidor para escuchar peticiones
 app.listen( process.env.PORT, () => {
 
